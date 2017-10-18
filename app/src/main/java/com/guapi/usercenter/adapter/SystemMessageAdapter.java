7729c8a1cacc62af7785f13faf7b4fcd6a246aac
long <18300072223@163.com>
2017-10-18 17:33:13 +0800

    修改Dialog圆角图片，消息列表时间显示

--- app/src/main/java/com/guapi/usercenter/adapter/SystemMessageAdapter.java
+++ app/src/main/java/com/guapi/usercenter/adapter/SystemMessageAdapter.java
@@ -4,12 +4,15 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.ewuapp.framework.common.utils.CheckUtil;
 import com.ewuapp.framework.common.utils.GlideUtil;
 import com.ewuapp.framework.view.adapter.BaseViewHolder;
 import com.ewuapp.framework.view.adapter.RecyclerAdapter;
 import com.ewuapp.framework.view.widget.CircleImageView;
 import com.guapi.R;
 import com.guapi.model.response.RefreshOneMessageResponse;
+import com.guapi.tool.DateUtil;
+import com.guapi.tool.Utils;
 
 import java.util.List;
 
@@ -56,9 +59,49 @@ public class SystemMessageAdapter extends RecyclerAdapter<RefreshOneMessageRespo
                 tvTitle.setText("好友");
             }
             tvContent.setText(object.getMs_content());
+            if (!CheckUtil.isNull(object.getMs_time())) {
+                tvTime.setText(getTimeStr(object.getMs_time()));
+            }
             tvTime.setText(object.getMs_time());
             GlideUtil.loadPicture(object.getSms_user_imag_url(), ivAvatar, R.mipmap.logo);
         }
     }
 
+    public String getTimeStr(String time) {
+        //create_time : 2017-09-23 19:11:59.0
+        String[] timeStr1 = time.split(" ");//timeStr1[0]=2017-09-23,timeStr[1]=19:11:59.0
+        String[] timeStr2 = timeStr1[0].split("-");//timeStr2[0]=2017,timeStr2[1]=09,timeStr2[2]=23
+        String monthStr = "";
+        String dayStr = "";
+        if (timeStr2[1].startsWith("0")) {
+            monthStr = timeStr2[1].substring(0);
+        } else {
+            monthStr = timeStr2[1];
+        }
+        if (timeStr2[2].startsWith("0")) {
+            dayStr = timeStr2[2].substring(1);
+        } else {
+            dayStr = timeStr2[2];
+        }
+        String str = "";
+        long[] times = Utils.getDistanceTimes(time, Utils.getDate("" + (System.currentTimeMillis() / 1000)));
+        if (DateUtil.isSameDay(DateUtil.formatDateMills(time, DateUtil.yyyy_MMddHHmmss))) {
+            if (times[3] < 1) {
+                if (times[4] < 1) {
+                    if (times[5] < 1) {
+                        str = times[5] + "秒前";
+                    } else {
+                        str = "刚才";
+                    }
+                } else {
+                    str = times[4] + "分钟前";
+                }
+            } else {
+                str = times[3] + "小时前";
+            }
+        } else {
+            str = timeStr2[0] + "年" + monthStr + "月" + dayStr + "日";
+        }
+        return str;
+    }
 }
